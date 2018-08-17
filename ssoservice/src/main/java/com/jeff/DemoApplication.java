32b9399d0fc85345eed6f4a1f6d4590627d300c8
youpengzhang@163.com <youpengzhang@163.com>
2018-08-17 17:21:18 +0800

    add application.properties

--- ssoservice/src/main/java/com/jeff/DemoApplication.java
+++ ssoservice/src/main/java/com/jeff/DemoApplication.java
@@ -2,13 +2,21 @@ package com.jeff;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Youpeng.Zhang on 2018/8/17.
  */
 
-
+@RestController
 @SpringBootApplication //Spring Boot核心注解，用于开启自动配置
 public class DemoApplication {
+
+    @RequestMapping("/index")
+    String index(){
+        return "Hello Spring Boot111";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
